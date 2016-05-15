e538ef694168400aaff910d3c2ae123e017120f4
mmosser <mmosser@uc.cl>
2016-05-15 13:49:31 -0400

    test request instagram

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,19 +6,6 @@ class ApplicationController < ActionController::Base
 
 
 
-
-
-def testHTTParty
-	
-	#access_token='2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
-	#q='snow'
-	
-	response=HTTParty.get("https://api.instagram.com/v1/tags/snow?access_token=2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402")
-	return response["data"]
-	
-end
-
-
 def requestWebWithAuth(uri)
 	access_token='2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
 
--- app/controllers/instagram/application_controller.rb
+++ app/controllers/instagram/application_controller.rb
@@ -10,6 +10,17 @@ def index
 
 end
 
+
+def testHTTParty
+	
+	#access_token='2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
+	#q='snow'
+	
+	response=HTTParty.get("https://api.instagram.com/v1/tags/snow?access_token=2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402")
+	render json: response["data"]
+	
+end
+
 def testUri
 	access_token='2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
 	q='snow'
