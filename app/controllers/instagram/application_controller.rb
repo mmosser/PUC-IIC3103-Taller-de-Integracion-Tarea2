dbc5467274df0d8fedb070f1c7a0902fd8333c5a
mmosser <mmosser@uc.cl>
2016-05-15 16:23:24 -0400

    test request instagram

--- app/controllers/instagram/application_controller.rb
+++ app/controllers/instagram/application_controller.rb
@@ -43,7 +43,8 @@ end
 def testHTTPartyWithParam
 	access_token='2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
 
-	response=HTTParty.get("https://api.instagram.com/v1/tags/#{param[:tag]}?access_token=#{access_token}")
+
+	response=HTTParty.get("https://api.instagram.com/v1/tags/#{params[:tag]}?access_token=#{access_token}")
 	render json: response
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     match '/test' => 'application#testUri', via: :get
     match '/test2' => 'application#testHTTParty', via: :get
     match '/test3' => 'application#testHTTPartyWithVariable', via: :get
-   # match '/testParam' => 'application#testParam', via: :post
+    match '/testParam' => 'application#testParam', via: :post
     match '/testMeta' => 'application#testHTTPartyWithParam', via: :post
 
     match '/tag/metadata/snow' => 'application#testRequestMetadata', via: :get
