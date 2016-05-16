f71640d64c230718914c67b1406e77aae21619ad
mmosser <mmosser@uc.cl>
2016-05-16 03:45:29 -0400

    version 2.0.3: debut getPosts

--- app/controllers/instagram/application_controller.rb
+++ app/controllers/instagram/application_controller.rb
@@ -74,7 +74,7 @@ def getPosts
 		if(response.code < 300)
 	    	render json: {
 				"metadata": false,
-				"posts": puts hashOrganized.to_json,
+				"posts": hashOrganized.to_json.to_s,
 				"version": "2.0.3" },
 				status: 200
 	  	else
